d8b81a24bae290d419382268fbd13c96e4677761
王自成 <wangzicheng@gillion.com.cn>
2022-11-11 23:37:39 +0800

    HelloWorld

---
+++ src/main/java/com/demo/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.junit.Test;
+
+/**
+ * @description:
+ * @author: wangzicheng
+ * @create: 2022-11-11 23:33
+ **/
+public class HelloWorld {
+
+    @Test
+    public void print(){
+        System.out.println("hello world");
+    }
+}
